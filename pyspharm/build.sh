e444d3598067383b9d6873d6caa8249645249f90
Andrew Dawson <ajdawson@acm.org>
2015-12-17 11:01:17 +0000

    Add pyspharm recipe.

---
+++ pyspharm/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Modify flags for build on osx
+if [ `uname` == Darwin ]; then
+    export LDFLAGS="-m64 -Wall -g -undefined dynamic_lookup -bundle"
+fi
+
+"$PYTHON" setup.py install <<"EOF"
+yes
+EOF
