53661f94fa774e5e4d782ecd6f5848a23bd11992
Andrew Dawson <ajdawson@acm.org>
2015-12-18 15:52:07 +0000

    Try and link static libquadmath.a for pyspharm os osx.

--- pyspharm/build.sh
+++ pyspharm/build.sh
@@ -2,7 +2,7 @@
 
 # Modify flags for build on osx
 if [ `uname` == Darwin ]; then
-    export LDFLAGS="-static -m64 -Wall -g -undefined dynamic_lookup -bundle -static-libgcc -static-libgfortran"
+    export LDFLAGS="-static /usr/local/lib/gcc/x86_64-apple-darwin13.3.0/4.9.1/libquadmath.a -m64 -Wall -g -undefined dynamic_lookup -bundle -static-libgcc -static-libgfortran"
 fi
 
 "$PYTHON" setup.py install <<"EOF"
