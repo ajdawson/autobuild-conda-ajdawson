17124cca9163b7efdfea43e565aed38372a6af6e
Andrew Dawson <ajdawson@acm.org>
2015-12-18 15:16:40 +0000

    Add static flag to pyspharm osx build.

--- pyspharm/build.sh
+++ pyspharm/build.sh
@@ -2,7 +2,7 @@
 
 # Modify flags for build on osx
 if [ `uname` == Darwin ]; then
-    export LDFLAGS="-m64 -Wall -g -undefined dynamic_lookup -bundle -static-libgfortran"
+    export LDFLAGS="-static -m64 -Wall -g -undefined dynamic_lookup -bundle -static-libgfortran"
 fi
 
 "$PYTHON" setup.py install <<"EOF"
